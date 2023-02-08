7c34139bd6bc907fd58923bad61bf847c8e72fac
iqbalaulawi123 <iqbalaulawi123@gmail.com>
2023-02-08 14:49:48 +0700

    modify header design

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -5,7 +5,7 @@ export default function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
             <div className="container-fluid">
-                <NavLink to="/home" className="navbar-brand">
+                <NavLink to="/home" className="navbar-brand fw-bold">
                     alterra-app
                 </NavLink>
                 <button
@@ -22,37 +22,40 @@ export default function Navbar(){
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <NavLink to="/" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         Home
                     </NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/about" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/about" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         About
                     </NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/todolist" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/todolist" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         Todolist (day 4-5)
                     </NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/form-daftar" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/form-daftar" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         Form-Daftar (day 5)
                     </NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/todolist-redux" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/todolist-redux" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         Todolist-redux (day 6-7)
                     </NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/todolist-redux-persist" className={({isActive})=>isActive ? "nav-link active":"nav-link"}>
+                    <NavLink to="/todolist-redux-persist" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
                         Todolist-redux-persist (day 6-7)
                     </NavLink>
                     </li>
-                    
-                    
+                    <li className="nav-item">
+                    <NavLink to="/test-api" className={({isActive})=>isActive ? "nav-link text-primary fw-bold":"nav-link"}>
+                        Data Fetching (day 7)
+                    </NavLink>
+                    </li>
                 </ul>
                 </div>
             </div>
